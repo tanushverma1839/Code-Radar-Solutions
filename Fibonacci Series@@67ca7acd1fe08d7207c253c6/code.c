32cd3a7ef4faf9b99dad38748389def28439a2df
tanushverma1839 <slektofficial@gmail.com>
2025-04-01 04:04:00 +0530

    Solution to Fibonacci Series

--- Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
+++ Fibonacci Series@@67ca7acd1fe08d7207c253c6/code.c
@@ -1,6 +1,6 @@
 #include<stdio.h>
 void fibonacciSeries(int n){
-    int n1 = 0,n2 = 0,sum = n1+n2;
+    int n1 = 0,n2 = 1,sum = 0;
     for(int i = 0;i < n;i++){
         printf("%d",sum);
         n1 = n2;
