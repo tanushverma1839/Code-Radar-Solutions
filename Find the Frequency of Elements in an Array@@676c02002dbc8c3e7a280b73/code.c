65f14f8847d37f00dd61419dc361e4510adc1991
tanushverma1839 <slektofficial@gmail.com>
2025-04-10 19:09:13 +0530

    Solution to Find the Frequency of Elements in an Array

--- Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
+++ Find the Frequency of Elements in an Array@@676c02002dbc8c3e7a280b73/code.c
@@ -1,33 +1,29 @@
 #include<stdio.h>
-int main(){
+
+int main() {
     int n;
-    scanf("%d",&n);
-    int arr[n],freq[n] = {0},count[n] = {0};
-    for(int i = 0;i < n;i++){
-        scanf("%d",&arr[i]);
+    scanf("%d", &n);
+    int arr[n];
+    int freq[n]; // To mark whether an element is already counted (0 -> not counted, 1 -> counted)
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+        freq[i] = 0; // Initially, mark all elements as not counted
     }
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < n;j++){
-            if(arr[i] != freq[j]){
-                freq[j] = arr[i];
+
+    for (int i = 0; i < n; i++) {
+        if (freq[i] == 1) {
+            continue; // If already counted, skip
+        }
+        int count = 1;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[i] == arr[j]) {
+                freq[j] = 1; // Mark duplicate elements
+                count++;
             }
         }
+        printf("%d %d\n", arr[i], count);
     }
-    int count2 = 0,x = 0;
-    for(int i = 0;i < n;i++){
-        for(int j = 0;j < n;j++){
-            if(freq[j] = arr[i]){
-                count2++;
-            }
-        }
-        count[x] = count2;
-        count2 = 0;
-    }
-    for(int i = 0;i < n;i++){
-        if(freq[i] == 0 || count[i]== 0){
-            continue;
-        }else{
-            printf("%d %d",freq[i],count[n]);
-        }
-    }
+
+    return 0;
 }
